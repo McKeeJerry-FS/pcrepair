a5d68464f518fc8f1e437fba3bff02318ec56a07
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-01-15 14:14:51 -0500

    Updating a new route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/test', function(req, res, next) {
+  res.send('This is a test route for users');
+});
+
 module.exports = router;
