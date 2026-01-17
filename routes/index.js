3bf804d3b535a28067a498f97112ec9ff9704edb
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-01-17 16:11:00 -0500

    Added new views to the website

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,34 @@
 var express = require('express');
+//const { act } = require('react');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'Express',
-    name: 'John Doe' 
+    title: 'Home',
+    name: 'John Doe',
+    activityPage: 'index' 
+  });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+    title: 'About',
+    name: 'John Doe',
+    activityPage: 'about' 
+  });
+});
+router.get('/services', function(req, res, next) {
+  res.render('services', { 
+    title: 'Services',
+    name: 'John Doe',
+    activityPage: 'services' 
+  });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { 
+    title: 'Contact',
+    name: 'John Doe',
+    activityPage: 'contact' 
   });
 });
 
